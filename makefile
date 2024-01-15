69da56ae5e1688d2ab6fcca9a856cacadcaf4500
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2024-01-15 16:30:49 +0100

    first commit

---
+++ makefile
@@ -0,0 +1,2 @@
+bin/analog: src/analog.cpp
+	c++ -std=c++14 -O3 src/analog.cpp -o bin/analog
---
+++ src/analog.cpp
@@ -0,0 +1,73 @@
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct Log {
+  std::string IP;
+  std::string username;
+  std::string authenticatedUser;
+  std::string time;
+  std::string timeZone;
+  std::string request;
+  char code[4];
+  unsigned int size;
+  std::string referer;
+  std::string UA;
+};
+
+int main(void) {
+
+  std::ifstream fin("Tests/logs/mini-anonyme.log");
+
+  if (!fin.is_open()) {
+    std::cout << "File open failed" << std::endl;
+    return -1;
+  }
+
+  std::vector<Log> logs;
+
+  while (!fin.eof()) {
+    Log log;
+    std::string tmp;
+
+    fin >> std::quoted(log.IP);
+    if (log.IP.empty())
+      break;
+
+    fin >> std::quoted(log.username);
+    fin >> std::quoted(log.authenticatedUser);
+    fin >> std::quoted(log.time);
+    fin >> std::quoted(log.timeZone);
+    fin >> std::quoted(log.request);
+
+    fin >> std::quoted(tmp);
+    std::strcpy(log.code, tmp.c_str());
+
+    fin >> std::quoted(tmp);
+    log.size = std::stoi(tmp);
+
+    fin >> std::quoted(log.referer);
+    fin >> std::quoted(log.UA);
+
+    logs.push_back(log);
+  }
+
+  for (auto &log : logs) {
+    std::cout << "============================" << std::endl;
+    std::cout << log.IP << std::endl;
+    std::cout << log.username << std::endl;
+    std::cout << log.authenticatedUser << std::endl;
+    std::cout << log.time << std::endl;
+    std::cout << log.timeZone << std::endl;
+    std::cout << log.request << std::endl;
+    std::cout << log.code << std::endl;
+    std::cout << log.size << std::endl;
+    std::cout << log.referer << std::endl;
+    std::cout << log.UA << std::endl;
+    std::cout << "============================" << std::endl;
+  }
+}
