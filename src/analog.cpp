07b4317b4cfc27e7321f52b969e9b8116f43c32d
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2024-01-15 23:33:43 +0100

    show first 10 document

--- src/analog.cpp
+++ src/analog.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstring>
 #include <fstream>
 #include <iomanip>
@@ -6,6 +7,7 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 
 using std::cout;
@@ -13,6 +15,7 @@ using std::endl;
 using std::string;
 
 const string BASE_URL = "http://intranet-if.insa-lyon.fr";
+const unsigned int MAX_SHOW = 10;
 
 struct Log {
   string IP;
@@ -119,10 +122,30 @@ int main(int argc, char *argv[]) {
     documents[log.requestUrl].addReferer(log.referer);
   }
 
-  for (auto &document : documents) {
+  // for (auto &document : documents) {
+  //   cout << document.first << " " << document.second.getHit() << endl;
+  //   for (auto &referer : document.second.getReferers()) {
+  //     cout << "\t" << referer.first << " " << referer.second << endl;
+  //   }
+  // }
+
+  std::vector<std::pair<string, document>> documentsSorted(documents.begin(),
+                                                           documents.end());
+
+  std::sort(documentsSorted.begin(), documentsSorted.end(),
+            [](const std::pair<string, document> &a,
+               const std::pair<string, document> &b) {
+              return a.second.getHit() > b.second.getHit();
+            });
+
+  unsigned int count = 0;
+  for (auto &document : documentsSorted) {
     cout << document.first << " " << document.second.getHit() << endl;
-    for (auto &referer : document.second.getReferers()) {
-      cout << "\t" << referer.first << " " << referer.second << endl;
-    }
+    ++count;
+    if (count >= MAX_SHOW)
+      break;
+    // for (auto &referer : document.second.getReferers()) {
+    //   cout << "\t" << referer.first << " " << referer.second << endl;
+    // }
   }
 }
