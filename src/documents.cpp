93b57a5bb7dc1eb2a415eb621e2ebdafc5c1c55e
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2024-01-16 12:45:58 +0100

    Refactor getSortedDucumentsByHit function in Documents class

--- src/documents.cpp
+++ src/documents.cpp
@@ -1,6 +1,23 @@
 #include "documents.h"
 #include <iostream>
 
+std::vector<std::pair<std::string, Document>>
+Documents::getSortedDucumentsByHit(unsigned int maxShow) const {
+  std::vector<std::pair<std::string, Document>> documentsSorted(
+      _documents.begin(), _documents.end());
+
+  std::sort(documentsSorted.begin(), documentsSorted.end(),
+            [](const std::pair<std::string, Document> &a,
+               const std::pair<std::string, Document> &b) {
+              return a.second.getHit() > b.second.getHit();
+            });
+
+  if (documentsSorted.size() > maxShow)
+    documentsSorted.resize(maxShow);
+
+  return documentsSorted;
+}
+
 std::ostream &operator<<(std::ostream &os, Documents &documents) {
 
   for (auto &document : documents._documents) {
--- src/documents.h
+++ src/documents.h
@@ -7,28 +7,13 @@
 #include <string>
 #include <vector>
 
-
 struct Documents {
   std::map<std::string, Document> _documents;
 
   Document &operator[](std::string url) { return _documents[url]; }
 
   std::vector<std::pair<std::string, Document>>
-  getSortedDucumentsByHit(unsigned int maxShow) const {
-    std::vector<std::pair<std::string, Document>> documentsSorted(
-        _documents.begin(), _documents.end());
-
-    std::sort(documentsSorted.begin(), documentsSorted.end(),
-              [](const std::pair<std::string, Document> &a,
-                 const std::pair<std::string, Document> &b) {
-                return a.second.getHit() > b.second.getHit();
-              });
-
-    if (documentsSorted.size() > maxShow)
-      documentsSorted.resize(maxShow);
-
-    return documentsSorted;
-  }
+  getSortedDucumentsByHit(unsigned int maxShow) const;
 };
 
 std::ostream &operator<<(std::ostream &os, Documents &documents);
